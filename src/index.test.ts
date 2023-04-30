cd0bea5b7186576fd8c08128077755d09b8921aa
Frank A. Krueger <fak@praeclarum.org>
2023-04-30 15:47:07 -0700

    Add tests

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import { testExport } from "./index";
+
+test("exports work", () => {
+  expect(testExport(42)).toBe(42);
+});
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+
+export function testExport(input: any): any {
+    return input;
+}
