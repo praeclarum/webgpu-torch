00a0b2cec1740c3d61c39805c226e72d77e9c8e7
Frank A. Krueger <fak@praeclarum.org>
2023-04-30 17:16:19 -0700

    Add requiresGrad

--- src/tensor.test.ts
+++ src/tensor.test.ts
@@ -6,4 +6,13 @@ test("create tensor with storage and dtype", () => {
     const tensor = new Tensor(storage, "float32");
     expect(tensor).toBeInstanceOf(Tensor);
     expect(tensor.untypedStorage).toBeInstanceOf(UntypedStorage);
+    expect(tensor.dtype).toBe("float32");
+});
+
+test("can toggle requiresGrad", () => {
+    const storage = new UntypedStorage();
+    const tensor = new Tensor(storage, "float32");
+    expect(tensor.requiresGrad).toBe(false);
+    tensor.requiresGrad = true;
+    expect(tensor.requiresGrad).toBe(true);
 });
--- src/tensor.ts
+++ src/tensor.ts
@@ -3,14 +3,24 @@ import { UntypedStorage } from './storage';
 export class Tensor {
     private _untypedStorage: UntypedStorage;
     private _dtype: string;
+    private _requiresGrad: boolean = false;
+
+    get untypedStorage(): UntypedStorage { 
+        return this._untypedStorage;
+    }
+    get dtype(): string {
+        return this._dtype;
+    }
+    get requiresGrad(): boolean {
+        return this._requiresGrad;
+    }
+    set requiresGrad(value: boolean) {
+        this._requiresGrad = value;
+    }
 
     constructor(untypedStorage: UntypedStorage, dtype: string) {
         this._untypedStorage = untypedStorage;
         this._dtype = dtype;
-    }
-
-    get untypedStorage(): UntypedStorage {
-        return this._untypedStorage;
     }
 }
 
