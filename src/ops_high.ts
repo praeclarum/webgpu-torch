39ab37e82fe0a85af149f149ddea6e3ba38c0ac9
Frank A. Krueger <fak@praeclarum.org>
2023-05-06 15:41:58 -0700

    Create high level ops 2

---
+++ src/ops_high.ts
@@ -0,0 +1,38 @@
+// High level ops
+
+import { IAutoFunction, shouldCreateGradient } from "./autograd";
+import { Tensor } from "./tensor";
+
+export function unary(func: IAutoFunction, input: Tensor): Tensor {
+    if (shouldCreateGradient(input)) {
+        return func.apply(input);
+    }
+    return func.forward([input]);
+}
+
+export function unaryWithAlpha(func: IAutoFunction, input: Tensor, alpha?: number): Tensor {
+    if (shouldCreateGradient(input)) {
+        return func.apply(input, alpha);
+    }
+    return func.forward([input, alpha]);
+}
+
+export function binary(func: IAutoFunction, input: Tensor, other: Tensor): Tensor {
+    if (input.shape.length !== other.shape.length) {
+        throw new Error(`Shape dimensions must match. Got ${input.shape} and ${other.shape}`);
+    }
+    if (shouldCreateGradient(input)) {
+        return func.apply(input, other);
+    }
+    return func.forward([input, other]);
+}
+
+export function binaryWithAlpha(func: IAutoFunction, input: Tensor, other: Tensor, alpha?: number): Tensor {
+    if (input.shape.length !== other.shape.length) {
+        throw new Error(`Shape dimensions must match. Got ${input.shape} and ${other.shape}`);
+    }
+    if (shouldCreateGradient(input)) {
+        return func.apply(input, other, alpha);
+    }
+    return func.forward([input, other, alpha]);
+}
