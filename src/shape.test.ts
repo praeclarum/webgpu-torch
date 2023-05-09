e48b525ae0119c5802eae52fef5275f595a13fb7
Frank A. Krueger <fak@praeclarum.org>
2023-05-09 09:22:00 -0700

    Fully test shape

--- src/shape.test.ts
+++ src/shape.test.ts
@@ -1,4 +1,4 @@
-import { Shape, Shapeish, getShape } from "./shape";
+import { Shape, Shapeish, getShape, shapeSize } from "./shape";
 
 test("number to shape", () => {
     expect(getShape(1)).toEqual([1]);
@@ -6,4 +6,8 @@ test("number to shape", () => {
 
 test("shape to shape", () => {
     expect(getShape([1, 2, 3])).toEqual([1, 2, 3]);
+});
+
+test("size of empty shape is 1", () => {
+    expect(shapeSize([])).toBe(1);
 });
--- src/shape.ts
+++ src/shape.ts
@@ -31,33 +31,3 @@ export function defaultStrides(shape: Shape): Strides {
     }
     return strides;
 }
-
-export function shapeGetAxis(shape: Shape, axis: number): number {
-    if (axis < 0) {
-        axis += shape.length;
-    }
-    if (axis < 0 || axis >= shape.length) {
-        throw new Error(`Axis ${axis} is out of bounds for shape ${shape}`);
-    }
-    return axis;
-}
-
-/*
-export function broadcastShape(leftShape: Shape, rightShape: Shape, rightStrides: Strides): {shape: Shape, strides: Strides} {
-    const leftRank = leftShape.length;
-    const rightRank = rightShape.length;
-    const newRank = Math.max(leftRank, rightRank);
-    const newShape = new Array(newRank);
-    const newStrides = new Array(newRank);
-    for (let i = 0; i < newRank; i++) {
-        const leftDim = leftShape[leftRank - 1 - i] || 1;
-        const rightDim = rightShape[rightRank - 1 - i] || 1;
-        if (leftDim !== rightDim && leftDim !== 1 && rightDim !== 1) {
-            throw new Error(`Cannot broadcast shapes ${leftShape} and ${rightShape}`);
-        }
-        newShape[newRank - 1 - i] = Math.max(leftDim, rightDim);
-        newStrides[newRank - 1 - i] = rightStrides[rightRank - 1 - i] || 0;
-    }
-    return {shape: newShape, strides: newStrides};
-}
-*/
