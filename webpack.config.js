2995e0c5f56e4256d1fa5e509b311f1839662ec4
Frank A. Krueger <fak@praeclarum.org>
2023-05-10 11:38:24 -0700

    Rename index to torch

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,37 @@
 const path = require("path");
-const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.ts", // Set the entry TypeScript file
-  devtool: "source-map",
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          keep_fnames: true, // Keep function names
-        },
-      }),
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    filename: "torch.js", // Set the output file name
-    path: path.resolve(__dirname, "dist"),
-    library: "torch",
-    libraryTarget: "umd",
-  },
+    entry: "./src/torch.ts", // Set the entry TypeScript file
+    devtool: "source-map",
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    keep_fnames: true, // Keep function names
+                },
+            }),
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    // rename index.d.ts to torch.d.ts
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "torch.js", // Set the output file name
+        library: "torch", // Set the exported library name
+        libraryTarget: "umd", // Set the target type of the exported library
+        globalObject: "torch", // Set the global object name
+    },
 };
