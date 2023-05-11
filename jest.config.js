a5e86e14aae12740c9c8620be89b5869ef2bc8a9
Frank A. Krueger <fak@praeclarum.org>
2023-05-10 18:38:35 -0700

    Return boolean from initWebGPUAsync()

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,7 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts", "!src/**/*.test.{ts,tsx}"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
--- src/webgpu.ts
+++ src/webgpu.ts
@@ -1,11 +1,19 @@
 import { discoverWebGPUDevicesAsync } from "./devices";
 
+/**
+ * Returns true if WebGPU is available, false otherwise.
+ * @returns true if WebGPU is available, false otherwise.
+ */
 export function hasWebGPU() {
     const anavigator = navigator as any;
     if (!anavigator.gpu) { return false; }
     return true;
 }
 
-export async function initWebGPUAsync() {
-    await discoverWebGPUDevicesAsync();
+/**
+ * Initializes WebGPU. This function must be called before any other functions.
+ * @returns true if WebGPU is available, false otherwise.
+ */
+export async function initWebGPUAsync(): Promise<boolean> {
+    return await discoverWebGPUDevicesAsync();
 }
