276aaeea42e9463f865ffb2a3ae6f3d775e0f0a8
Frank A. Krueger <fak@praeclarum.org>
2023-06-21 20:41:23 -0700

    Add test for deep graphs

--- src/graph.test.ts
+++ src/graph.test.ts
@@ -1,5 +1,6 @@
 import { ComputedNode } from "./graph";
 import { tensor } from "./ops_artisanal";
+import { Tensor } from "./tensor";
 
 test("array tensors are source nodes", async () => {
     const x = tensor([1, 2, 3]);
@@ -14,4 +15,19 @@ test("unop tensors are computed nodes", async () => {
     expect(await y.node.storage.toArrayAsync(y.dtype)).toEqual([-1, -2, -3]);
     const node = y.node as ComputedNode;
     expect(node.inputs).toEqual([x.node]);
+});
+
+test("deep equal-width graphs use minimal buffers", async () => {
+    const x = tensor([1, 2, 3]);
+    let y = x;
+    const ys: Tensor[] = [];
+    const depth = 101;
+    for (let i = 0; i < depth; i++) {
+        y = y.neg();
+        ys.push(y);
+    }
+    expect(y.node.isComputed).toBe(true);
+    expect(await y.node.storage.toArrayAsync(y.dtype)).toEqual([-1, -2, -3]);
+    const node = y.node as ComputedNode;
+    expect(node.inputs).toEqual([ys[depth - 2].node]);
 });
