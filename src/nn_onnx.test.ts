2dfe056fa94d21693cc24c01a0edd3f76889aad3
Frank A. Krueger <fak@praeclarum.org>
2023-06-27 13:11:12 -0700

    Run onnx matmul test

--- src/nn_onnx.test.ts
+++ src/nn_onnx.test.ts
@@ -2,16 +2,16 @@ import { ones } from "./factories";
 import { ONNXModule } from "./nn_onnx";
 import { Tensor } from "./tensor";
 
-// test("load from url", async () => {
-//     const model = await ONNXModule.loadUrlAsync("https://praeclarum.org/webgpu-torch/tests/models/matmul.onnx");
-//     expect(model).toBeInstanceOf(ONNXModule);
-//     const a = ones([2, 2]);
-//     const b = ones([2, 2]);
-//     const c = model.forward([a, b])[0];
-//     expect(c).toBeInstanceOf(Tensor);
-//     expect(c.shape).toEqual([2, 2]);
-//     expect(await c.toArrayAsync()).toEqual([[2, 2], [2, 2]]);
-// });
+test("load from url", async () => {
+    const model = await ONNXModule.loadUrlAsync("https://praeclarum.org/webgpu-torch/tests/models/matmul.onnx");
+    expect(model).toBeInstanceOf(ONNXModule);
+    const a = ones([2, 3]);
+    const b = ones([3, 2]);
+    const c = model.forward([a, b])[0];
+    expect(c).toBeInstanceOf(Tensor);
+    expect(c.shape).toEqual([2, 2]);
+    expect(await c.toArrayAsync()).toEqual([[3, 3], [3, 3]]);
+});
 
 // DEV TESTS
 
