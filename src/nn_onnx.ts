de11c7f81ec32455e3fe366a74c93bf0db309987
Frank A. Krueger <fak@praeclarum.org>
2023-06-27 12:21:11 -0700

    Deconstruct an OONx model into its parts

--- src/nn_onnx.ts
+++ src/nn_onnx.ts
@@ -3,8 +3,14 @@ import { onnx } from "./onnx";
 import { fetch } from "cross-fetch";
 
 export class ONNXModule extends Module {
-    constructor(model: onnx.ModelProto) {
+    readonly inputs: onnx.IValueInfoProto[];
+    readonly outputs: onnx.IValueInfoProto[];
+    readonly nodes: onnx.INodeProto[];
+    constructor(inputs: onnx.IValueInfoProto[], outputs: onnx.IValueInfoProto[], nodes: onnx.INodeProto[]) {
         super();
+        this.inputs = inputs;
+        this.outputs = outputs;
+        this.nodes = nodes;
     }
     static async loadUrlAsync(url: string): Promise<ONNXModule> {
         const response = await fetch(url);
@@ -16,6 +22,23 @@ export class ONNXModule extends Module {
         return this.loadModel(model);
     }
     static loadModel(model: onnx.ModelProto): ONNXModule {
-        return new ONNXModule(model);
+        const graph = model.graph;
+        if (!graph) {
+            throw new Error("Model does not contain a graph");
+        }
+        const inputs = graph.input;
+        if (!inputs) {
+            throw new Error("Model graph does not contain inputs");
+        }
+        const outputs = graph.output;
+        if (!outputs) {
+            throw new Error("Model graph does not contain outputs");
+        }
+        const nodes = graph.node;
+        if (!nodes) {
+            throw new Error("Model graph does not contain nodes");
+        }
+        // const initializer = graph.initializer;
+        return new ONNXModule(inputs, outputs, nodes);
     }
 }
