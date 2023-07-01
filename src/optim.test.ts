4a6309a95c6879524242d007cbd6630432ee6648
Frank A. Krueger <fak@praeclarum.org>
2023-06-30 18:10:28 -0700

    Building train loop test

--- src/optim.test.ts
+++ src/optim.test.ts
@@ -2,7 +2,10 @@
 import { SGD, required } from "./optim";
 import { Tensor } from "./tensor";
 import { sigmoid } from "./ops_opgen";
-import { noGrad } from "./autograd";
+import { Sequential } from "./nn_module";
+import { Linear } from "./nn_basic";
+import { ReLU } from "./nn_opgen";
+import { ones } from "./factories";
 
 test("optimizer ctor with empty param list", () => {
     expect(() => new SGD([], 0.1)).toThrow();
@@ -60,3 +63,47 @@ test("sgd step with params with grads", async () => {
     const paramValue = await params[0].toArrayAsync() as number[];
     expect(paramValue[0]).toBeCloseTo(1.5 - 0.1 * 0.14914645);
 });
+
+test("sgd mlp train loop", async () => {
+    const hiddenSize = 31;
+    const model = new Sequential(
+        new Linear(3, hiddenSize),
+        new ReLU(),
+        new Linear(hiddenSize, hiddenSize),
+        new ReLU(),
+        new Linear(hiddenSize, 1),
+    );
+    const batchSize = 2;
+    const radius = ones([batchSize, 1]);
+    const sphere = (batchedPoints: Tensor) => 
+        batchedPoints.pow(2).sum(1, true).sqrt().sub(radius);
+    const sphereA = (batchedPoints: number[][]) => {
+        const result: number[] = [];
+        for (let i = 0; i < batchedPoints.length; i++) {
+            const [x, y, z] = batchedPoints[i];
+            const d = Math.sqrt(x*x + y*y + z*z) - 1;
+            result.push(d);
+        }
+        return result;
+    }
+    const maxSteps = 1;
+    const optimizer = new SGD(Array.from(model.parameters()), 0.1);
+    for (let stepIndex = 0; stepIndex < maxSteps; stepIndex++) {
+        const pointsArray: number[][] = [];
+        for (let batchIndex = 0; batchIndex < batchSize; batchIndex++) {
+            pointsArray.push([(Math.random()-0.5)*4, (Math.random()-0.5)*4, (Math.random()-0.5)*4]);
+        }
+        const points = new Tensor(pointsArray);
+        const expectedDistances = sphere(points);
+        // console.log("expectedDistances", await expectedDistances.toArrayAsync());
+        // const expectedDistancesA = sphereA(pointsArray);
+        // console.log("expectedDistancesA", expectedDistancesA);
+        const predictedDistances = model.forward(points);
+        // console.log("predictedDistances", await predictedDistances.toArrayAsync());
+        const loss = predictedDistances.sub(expectedDistances).pow(2).mean();
+        // console.log("loss", await loss.toArrayAsync());
+        // loss.backward();
+        // optimizer.step();
+    }
+});
+
